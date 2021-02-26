f2a59ef2eaba2e300757352dcb22b2ddaf603a17
Daniel-Bichof <Daniel-Bichof@gmail.com>
2021-02-26 11:28:14 -0300

    dia 4

---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
+import styles from '../styles/components/Profile.module.css'
+
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/Daniel-Bichof.png" alt='Daniel Bichof' />
+            <div>
+                <strong>Daniel Bichof</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level { level }
+
+                </p>
+            </div>
+        </div>
+
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/global.css'
+
+import { ChallengesProvider } from '../contexts/ChallengesContext'
+import React from 'react'
+
+function MyApp({ Component, pageProps }) {
+
+  return (
+      <ChallengesProvider>
+          {<Component {...pageProps} />}
+      </ChallengesProvider>
+      
+  )
+}
+
+export default MyApp
